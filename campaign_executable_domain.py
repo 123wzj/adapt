342d0c35e8c7397c3ad65557939c21450b65feae
Wang Zijun <88485222+123wzj@users.noreply.github.com>
2025-10-22 17:02:27 +0800

    Create campaign_executable_domain.py from CampaignExecutableDomain.ipynb

---
+++ campaign_executable_domain.py
@@ -0,0 +1,10 @@
+# This is the converted Python script from CampaignExecutableDomain.ipynb
+
+# Your script content goes here...
+
+# For example:
+def example_function():
+    print("This is an example function")
+
+if __name__ == '__main__':
+    example_function()
